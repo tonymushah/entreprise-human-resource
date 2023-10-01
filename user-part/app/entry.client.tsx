a9d92a4d9730a029bb10c48ab3ec67b7176da81e
tonymushah <tonyramanantsalama@gmail.com>
2023-10-01 19:33:00 +0300

    Generated user front project

---
+++ user-part/app/entry.client.tsx
@@ -0,0 +1,43 @@
+/**
+ * By default, Remix will handle hydrating your app on the client for you.
+ * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
+ * For more information, see https://remix.run/file-conventions/entry.client
+ */
+
+import { RemixBrowser } from "@remix-run/react";
+import { startTransition, StrictMode, useState } from "react";
+import { hydrateRoot } from "react-dom/client";
+import createEmotionCache, { defaultCache } from "./createEmotionCache";
+import { ClientStyleContext } from "./context";
+import { CacheProvider } from "@emotion/react";
+
+interface ClientCacheProviderProps {
+  children: React.ReactNode;
+}
+
+function ClientCacheProvider({ children }: ClientCacheProviderProps) {
+  const [cache, setCache] = useState(defaultCache)
+
+  function reset() {
+    setCache(createEmotionCache())
+  }
+
+  return (
+    <ClientStyleContext.Provider value={{ reset }}>
+      <CacheProvider value={cache}>{children}</CacheProvider>
+    </ClientStyleContext.Provider>
+  )
+}
+
+startTransition(() => {
+  hydrateRoot(
+    document,
+    <StrictMode>
+      <ClientCacheProvider>
+        <RemixBrowser />
+      </ClientCacheProvider>
+    </StrictMode>
+  );
+});
+
+
---
+++ user-part/app/root.tsx
@@ -0,0 +1,104 @@
+import { ChakraProvider, Box, Heading } from "@chakra-ui/react";
+import type { MetaFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  isRouteErrorResponse,
+  useRouteError,
+} from "@remix-run/react";
+
+export const meta: MetaFunction = () => ([{
+  charset: "utf-8",
+  viewport: "width=device-width,initial-scale=1",
+}]);
+
+function Document({
+  children,
+  title = "App title",
+}: {
+  children: React.ReactNode;
+  title?: string;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <title>{title}</title>
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+export default function App() {
+  // throw new Error("💣💥 Booooom");
+
+  return (
+    <Document>
+      <ChakraProvider>
+        <Outlet />
+      </ChakraProvider>
+    </Document>
+  );
+}
+
+// How ChakraProvider should be used on CatchBoundary
+export function ErrorBoundary() {
+  const caught = useRouteError();
+
+  if (isRouteErrorResponse(caught)) {
+    return (
+      <Document title={`${caught.status} ${caught.statusText}`}>
+        <ChakraProvider>
+          <Box>
+            <Heading as="h1" bg="purple.600">
+              [CatchBoundary]: {caught.status} {caught.statusText}
+            </Heading>
+          </Box>
+        </ChakraProvider>
+      </Document>
+    )
+  } else if (caught instanceof Error) {
+    return (
+      <SpecialErrorBoundary error={caught} />
+    )
+  } else {
+    return (
+      <Document title="Error!">
+        <ChakraProvider>
+          <Box>
+            <Heading as="h1" bg="blue.500">
+              [ErrorBoundary]: There was an unknown error
+            </Heading>
+          </Box>
+        </ChakraProvider>
+      </Document>
+    )
+  }
+
+}
+
+// How ChakraProvider should be used on ErrorBoundary
+function SpecialErrorBoundary({ error }: { error: Error }) {
+  return (
+    <Document title="Error!">
+      <ChakraProvider>
+        <Box>
+          <Heading as="h1" bg="blue.500">
+            [ErrorBoundary]: There was an error: {error.message}
+          </Heading>
+        </Box>
+      </ChakraProvider>
+    </Document>
+  );
+}
