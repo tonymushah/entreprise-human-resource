5e2ddb59a5ffe33df619f2f1efba3be5383a3319
tonymushah <tonyramanantsalama@gmail.com>
2023-10-01 19:56:59 +0300

    added Java Backend

---
+++ human-resource/src/main/java/itu/etu001844/humanresource/HumanResourceApplication.java
@@ -0,0 +1,13 @@
+package itu.etu001844.humanresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourceApplication.class, args);
+	}
+
+}
---
+++ human-resource/src/test/java/itu/etu001844/humanresource/HumanResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package itu.etu001844.humanresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
