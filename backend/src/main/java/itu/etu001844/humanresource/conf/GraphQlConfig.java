a3cf8daceadbd787730eb306afec7f072a350992
tonymushah <tonyramanantsalama@gmail.com>
2023-10-02 15:52:54 +0300

    Starting building the main GraphQL schema

---
+++ backend/src/main/java/itu/etu001844/humanresource/conf/GraphQlConfig.java
@@ -0,0 +1,15 @@
+package itu.etu001844.humanresource.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+
+import graphql.scalars.ExtendedScalars;
+
+@Configuration
+public class GraphQlConfig {
+    @Bean
+    public RuntimeWiringConfigurer runtimeWiringConfigurer() {
+        return wiringBuilder -> wiringBuilder.scalar(ExtendedScalars.Date).scalar(ExtendedScalars.UUID);
+    }
+}
