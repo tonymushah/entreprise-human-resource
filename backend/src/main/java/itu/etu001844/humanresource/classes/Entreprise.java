1ae9b6c16b1dcc0e73f5dac54f4561bf33cae230
tonymushah <tonyramanantsalama@gmail.com>
2023-10-04 20:31:40 +0300

    Added Entreprise.java

---
+++ backend/src/main/java/itu/etu001844/humanresource/classes/Entreprise.java
@@ -0,0 +1,41 @@
+package itu.etu001844.humanresource.classes;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Entreprise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    private String address;
+    public UUID getId() {
+        return id;
+    }
+    public void setId(UUID id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    protected Entreprise() {
+    }
+    public Entreprise(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+}
