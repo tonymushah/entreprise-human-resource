e4e6cda57b43cfc6772da64522889b5c199005ba
tonymushah <tonyramanantsalama@gmail.com>
2023-10-04 20:44:20 +0300

    Created ExperienceType.java

---
+++ backend/src/main/java/itu/etu001844/humanresource/classes/ExperienceType.java
@@ -0,0 +1,33 @@
+package itu.etu001844.humanresource.classes;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class ExperienceType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    protected ExperienceType() {
+    }
+    public UUID getId() {
+        return id;
+    }
+    public void setId(UUID id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public ExperienceType(String name) {
+        this.name = name;
+    }
+}
