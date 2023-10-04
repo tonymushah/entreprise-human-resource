4b6c1036daefcd44a3c3eb41a00979c81bbeb948
tonymushah <tonyramanantsalama@gmail.com>
2023-10-04 17:15:05 +0300

    Builded schema types
    Signed-off-by: tonymushah <tonyramanantsalama@gmail.com>

--- backend/src/main/java/itu/etu001844/humanresource/conf/GraphQlConfig.java
+++ backend/src/main/java/itu/etu001844/humanresource/conf/GraphQlConfig.java
@@ -4,12 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.graphql.execution.RuntimeWiringConfigurer;
 
+import graphql.Scalars;
 import graphql.scalars.ExtendedScalars;
+import graphql.schema.GraphQLScalarType;
 
 @Configuration
 public class GraphQlConfig {
     @Bean
     public RuntimeWiringConfigurer runtimeWiringConfigurer() {
-        return wiringBuilder -> wiringBuilder.scalar(ExtendedScalars.Date).scalar(ExtendedScalars.UUID);
+        GraphQLScalarType address = ExtendedScalars
+                .newAliasedScalar("Address")
+                .aliasedScalar(Scalars.GraphQLString)
+                .build();
+        return wiringBuilder -> wiringBuilder.scalar(ExtendedScalars.Date).scalar(ExtendedScalars.UUID).scalar(address);
     }
 }
